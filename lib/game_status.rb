66389dd939e9a9822c801ca3e4d9712f13d14ce0
Sarah Dahnke <sarahrosedahnke@gmail.com>
2017-11-27 01:42:23 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,26 @@ WIN_COMBINATIONS = [
 [0,4,8],#diagonal l to r
 [2,4,6]#diagonal r to l
 ]
+
+for each win_combination in WIN_COMBINATIONS
+  # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
+  # grab each index from the win_combination that composes a win.
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+  win_index_4 = win_combination[3]
+  win_index_5 = win_combination[4]
+  win_index_6 = win_combination[5]
+  win_index_7 = win_combination[6]
+  win_index_8 = win_combination[7]
+
+  position_1 = board[win_index_1] # load the value of the board at win_index_1
+  position_2 = board[win_index_2] # load the value of the board at win_index_2
+  position_3 = board[win_index_3] # load the value of the board at win_index_3
+
+  if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return win_combination # return the win_combination indexes that won.
+  else
+    false
+  end
+end
