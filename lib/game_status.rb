7a34c3435dc705c4b48ad45f480e366f11ba82ed
Sarah Dahnke <sarahrosedahnke@gmail.com>
2017-11-27 02:18:58 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,21 +15,39 @@ WIN_COMBINATIONS = [
 [2,4,6]#diagonal r to l
 ]
 
-def won?(board)
+# def won?(board)
+#   WIN_COMBINATIONS.each do |win_combination|
+#     win_index_1 = win_combination[0]
+#     win_index_2 = win_combination[1]
+#     win_index_3 = win_combination[2]
+#     position_1 = board[win_index_1]
+#     position_2 = board[win_index_2]
+#     position_3 = board[win_index_3]
+#     if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+#       return win_combination
+#     else
+#       return false
+#     end
+#   end
+# end
+def won? (board)
   WIN_COMBINATIONS.each do |win_combination|
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
+
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
-    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+
+    if [position_1, position_2, position_3].all? {|symbol| symbol == "X"}
       return win_combination
-    else
-      return false
-    end
-  end
-end
+    elsif [position_1, position_2, position_3].all? {|symbol| symbol == "O"}
+       return win_combination
+     end
+   end
+   false
+ end
 
 def full?(board)
   !board.include?(" ") #if board does not include a space
