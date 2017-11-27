d0e045cf9599467def4db6d21ff0fb5189db0370
Sarah Dahnke <sarahrosedahnke@gmail.com>
2017-11-27 01:43:10 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,11 +21,11 @@ for each win_combination in WIN_COMBINATIONS
   win_index_1 = win_combination[0]
   win_index_2 = win_combination[1]
   win_index_3 = win_combination[2]
-  win_index_4 = win_combination[3]
-  win_index_5 = win_combination[4]
-  win_index_6 = win_combination[5]
-  win_index_7 = win_combination[6]
-  win_index_8 = win_combination[7]
+  # win_index_4 = win_combination[3]
+  # win_index_5 = win_combination[4]
+  # win_index_6 = win_combination[5]
+  # win_index_7 = win_combination[6]
+  # win_index_8 = win_combination[7]
 
   position_1 = board[win_index_1] # load the value of the board at win_index_1
   position_2 = board[win_index_2] # load the value of the board at win_index_2
